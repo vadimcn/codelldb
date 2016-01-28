085c082694213151cf006ad6237823e995651c51
Vadim Chugunov <vadimcn@gmail.com>
2016-01-28 12:41:04 -0800

    WIP

--- adapter/debugsession.py
+++ adapter/debugsession.py
@@ -107,7 +107,7 @@ class DebugSession:
         target_args = args.get('args', None)
         if target_args is not None:
             target_args = [str(arg) for arg in target_args]
-
+        # environment
         env = args.get('env', None)
         envp = None
         if (env is not None): # Convert dict to a list of 'key=value' strings

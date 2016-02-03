7058dad87f47edb5b416a946506228b7b4eecfea
Vadim Chugunov <vadimcn@gmail.com>
2016-02-03 13:21:36 -0800

    back to node launcher

---
+++ adapter.js
@@ -0,0 +1,22 @@
+var cp = require('child_process');
+var process = require('process');
+var fs = require('fs')
+
+// Point this to a file or a tty to see adapter's debug spew
+var lldb_log = 'ignore'
+//var lldb_log = fs.openSync('/dev/ttys001', 'w');
+
+var lldb = cp.spawn('lldb', ['-b', '-O', 'script import adapter; adapter.run_stdio_session(3,4)'], {
+    // LLDB has readline attached to its stdin and sometimes spews debug messages to stdout,
+    // all of which would interfere with debug session messaging.
+    // Instead, we create two new pipes on fds 3 and 4 and connect them to launcher's stdin and stdout
+    stdio: ['ignore', lldb_log, lldb_log, 'pipe', 'pipe'],
+    cwd: __dirname
+});
+process.stdin.pipe(lldb.stdio[3]);
+lldb.stdio[4].pipe(process.stdout);
+
+// When lldb exits, we exit too
+lldb.on('exit', (code) => {
+    process.exit(code);
+});
--- adapter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec lldb -b -O "command script import $(dirname $0)/adapter" -O "script adapter.run_stdio_session()"

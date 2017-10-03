400b0733e711707c8ed607570c5b2f23df62941b
Vadim Chugunov <vadimcn@gmail.com>
2017-10-03 13:31:05 -0700

    Factor out processList().

--- extension/extension.ts
+++ extension/extension.ts
@@ -4,11 +4,10 @@ import {
     ExtensionContext, Disposable, QuickPickItem, Uri, Event, EventEmitter,
     DebugConfiguration, DebugSession
 } from 'vscode';
-import * as cp from 'child_process';
-import * as path from 'path';
-import { format } from 'util';
 import { DebugProtocol } from 'vscode-debugprotocol';
+import * as path from 'path';
 import * as startup from './startup';
+import * as util from './util';
 
 export function activate(context: ExtensionContext) {
     new Extension(context);
@@ -141,48 +140,8 @@ class Extension {
         }
     }
 
-    async  pickProcess(currentUserOnly: boolean): Promise<number> {
-        let is_windows = process.platform == 'win32';
-        var command: string;
-        if (!is_windows) {
-            if (currentUserOnly)
-                command = 'ps x';
-            else
-                command = 'ps ax';
-        } else {
-            if (currentUserOnly)
-                command = 'tasklist /V /FO CSV /FI "USERNAME eq ' + process.env['USERNAME'] + '"';
-            else
-                command = 'tasklist /V /FO CSV';
-        }
-        let stdout = await new Promise<string>((resolve, reject) => {
-            cp.exec(command, (error, stdout, stderr) => {
-                if (error) reject(error);
-                else resolve(stdout)
-            })
-        });
-        let lines = stdout.split('\n');
-        let items: (QuickPickItem & { pid: number })[] = [];
-
-        var re: RegExp, idx: number[];
-        if (!is_windows) {
-            re = /^\s*(\d+)\s+.*?\s+.*?\s+.*?\s+(.*)()$/;
-            idx = [1, 2, 3];
-        } else {
-            // name, pid, ..., window title
-            re = /^"([^"]*)","([^"]*)",(?:"[^"]*",){6}"([^"]*)"/;
-            idx = [2, 1, 3];
-        }
-        for (var i = 1; i < lines.length; ++i) {
-            let groups = re.exec(lines[i]);
-            if (groups) {
-                let pid = parseInt(groups[idx[0]]);
-                let name = groups[idx[1]];
-                let descr = groups[idx[2]];
-                let item = { label: format('%d: %s', pid, name), description: descr, pid: pid };
-                items.unshift(item);
-            }
-        }
+    async pickProcess(currentUserOnly: boolean): Promise<number> {
+        let items = util.getProcessList(currentUserOnly);
         let item = await window.showQuickPick(items);
         if (item) {
             return item.pid;
--- extension/util.ts
+++ extension/util.ts
@@ -1,10 +1,60 @@
 'use strict';
+import * as cp from 'child_process';
+import { format } from 'util';
+import { QuickPickItem } from 'vscode';
 
-let expandVarRegex = new RegExp('\\$\\{(?:([^:}]+):)?([^}]+)\\}', 'g');
+let expandVarRegex = /\$\{(?:([^:}]+):)?([^}]+)\}/g;
 
 export function expandVariables(str: string, expander: (type: string, key: string) => string): string {
     let result = str.replace(expandVarRegex, (all: string, type: string, key: string): string => {
         return expander(type, key);
     });
     return result;
+}
+
+export async function getProcessList(currentUserOnly: boolean):
+    Promise<(QuickPickItem & { pid: number })[]> {
+
+    let is_windows = process.platform == 'win32';
+    var command: string;
+    if (!is_windows) {
+        if (currentUserOnly)
+            command = 'ps x';
+        else
+            command = 'ps ax';
+    } else {
+        if (currentUserOnly)
+            command = 'tasklist /V /FO CSV /FI "USERNAME eq ' + process.env['USERNAME'] + '"';
+        else
+            command = 'tasklist /V /FO CSV';
+    }
+    let stdout = await new Promise<string>((resolve, reject) => {
+        cp.exec(command, (error, stdout, stderr) => {
+            if (error) reject(error);
+            else resolve(stdout)
+        })
+    });
+    let lines = stdout.split('\n');
+    let items = [];
+
+    var re: RegExp, idx: number[];
+    if (!is_windows) {
+        re = /^\s*(\d+)\s+.*?\s+.*?\s+.*?\s+(.*)()$/;
+        idx = [1, 2, 3];
+    } else {
+        // name, pid, ..., window title
+        re = /^"([^"]*)","([^"]*)",(?:"[^"]*",){6}"([^"]*)"/;
+        idx = [2, 1, 3];
+    }
+    for (var i = 1; i < lines.length; ++i) {
+        let groups = re.exec(lines[i]);
+        if (groups) {
+            let pid = parseInt(groups[idx[0]]);
+            let name = groups[idx[1]];
+            let descr = groups[idx[2]];
+            let item = { label: format('%d: %s', pid, name), description: descr, pid: pid };
+            items.unshift(item);
+        }
+    }
+    return items;
 }

eb794f3365cfad47253ab2083dc7cb5aeea7697a
Vadim Chugunov <vadimcn@gmail.com>
2017-12-17 17:31:41 -0800

    Fix #80

--- adapter/__init__.py
+++ adapter/__init__.py
@@ -3,7 +3,8 @@ import sys
 PY2 = sys.version_info[0] == 2
 if PY2:
     is_string = lambda v: isinstance(v, basestring)
-    to_lldb_str = lambda s: s.encode('utf8', 'backslashreplace')
+    # python2-based LLDB accepts utf8-encoded ascii strings only.
+    to_lldb_str = lambda s: s.encode('utf8', 'backslashreplace') if isinstance(s, unicode) else s
     from_lldb_str = lambda s: s.decode('utf8', 'replace')
     xrange = xrange
 else:

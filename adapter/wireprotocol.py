80e907d6068567701221c57b833d1c9d12e13c8a
Vadim Chugunov <vadimcn@gmail.com>
2018-04-05 20:17:37 -0700

    Fix Windows fallout.

--- adapter/wireprotocol.py
+++ adapter/wireprotocol.py
@@ -22,8 +22,9 @@ class DebugServer(WorkerThread):
             while not self.stopping:
                 clen = self.recv_headers()
                 data = self.recv_body(clen)
+                data = data.decode('utf8')
                 log.debug('rx: %s', data)
-                message = json.loads(data, encoding='utf8')
+                message = json.loads(data)
                 self.handle_message(message)
             log.info('Shutting down')
         except StopIteration: # Thrown when read() returns 0
@@ -71,7 +72,8 @@ class DebugServer(WorkerThread):
 
     json_separators = (',', ':')
     def send_message(self, message):
-        data = json.dumps(message, encoding='utf8', separators=self.json_separators)
+        data = json.dumps(message, separators=self.json_separators)
         log.debug('tx: %s', data)
+        data = data.encode('utf8')
         self.with_timeout(self.write_all, b'Content-Length: %d\r\n\r\n' % len(data))
         self.with_timeout(self.write_all, data)
--- tools/set_lldb_path.py
+++ tools/set_lldb_path.py
@@ -5,5 +5,5 @@ import subprocess
 lldb = os.environ.get('LLDB_EXECUTABLE', 'lldb')
 
 out = subprocess.check_output([lldb, '-P'])
-sys.path.append(out.strip())
+sys.path.append(out.decode('utf8').strip())
 sys.path.append('.')

07ea75c5e5f0294a9ca184bc44619a83e5cda38b
Vadim Chugunov <vadimcn@gmail.com>
2018-10-17 14:53:52 -0700

    Make python debugging work with latest Python extension.

--- tools/debug_launcher.py
+++ tools/debug_launcher.py
@@ -3,12 +3,12 @@
 import sys
 import subprocess
 
-args = ['*'] + sys.argv[3:]
+args = sys.argv[1:]
 script = [
-  "import sys, runpy, __main__",
+  "import sys,runpy,__main__",
   "sys.orig_main = __main__",
   "sys.argv=['%s']" % "','".join(args),
-  "runpy.run_module('ptvsd', alter_sys=True, run_name='__main__')"
+  "runpy.run_path('%s', run_name='__main__')" % sys.argv[1]
 ]
 command = ['lldb-6.0', '-b', '-O', 'script ' + '; '.join(script)]
 subprocess.call(command)

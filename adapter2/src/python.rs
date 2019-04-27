70a3e2f10bda7fdf46b88a2d88ef692534c4dd9f
Vadim Chugunov <vadimcn@gmail.com>
2019-04-26 17:13:35 -0700

    Auto-import the debugger module.

--- adapter2/src/python.rs
+++ adapter2/src/python.rs
@@ -39,18 +39,17 @@ impl PythonInterface {
     pub fn new(
         interpreter: SBCommandInterpreter, send_event: Box<Fn(EventBody) + Send>,
     ) -> Result<Box<PythonInterface>, Error> {
-        let mut init_script = env::current_exe()?;
-        init_script.set_file_name("codelldb.py");
-
-        // Import codelldb.py
+        let current_exe = env::current_exe()?;
         let mut command_result = SBCommandReturnObject::new();
+
+        // Import debugger.py into script interpreter's namespace.
+        // This also adds our bin directory to sys.path, so we can import the rest of our modules below.
+        let init_script = current_exe.with_file_name("debugger.py");
         let command = format!("command script import '{}'", init_script.to_str()?);
         interpreter.handle_command(&command, &mut command_result, false);
         info!("{:?}", command_result);
 
-        // Import rust.py
-        let mut rust_formatters = init_script.clone();
-        rust_formatters.set_file_name("rust.py");
+        let rust_formatters = current_exe.with_file_name("rust.py");
         let command = format!("command script import '{}'", rust_formatters.to_str()?);
         interpreter.handle_command(&command, &mut command_result, false);
         info!("{:?}", command_result);

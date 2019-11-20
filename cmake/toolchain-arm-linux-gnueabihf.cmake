e991543e4ed2b89d9ed5290f6de01f447f5edcf8
Vadim Chugunov <vadimcn@gmail.com>
2019-11-19 22:26:52 -0800

    Add support for armv7.

---
+++ cmake/toolchain-arm-linux-gnueabihf.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_STRIP arm-linux-gnueabihf-strip)
+set(LLVM_TRIPLE armv7-unknown-linux-gnueabihf)
---
+++ cmake/toolchain-x86_64-windows-msvc.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_C_COMPILER cl)
+set(CMAKE_CXX_COMPILER cl)
+set(LLVM_TRIPLE x86_64-pc-windows-msvc)

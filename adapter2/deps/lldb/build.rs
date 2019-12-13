01ad03217f6eafeb1375d2bc46891e6c1c939be5
Vadim Chugunov <vadimcn@gmail.com>
2019-12-13 00:06:34 -0800

    Use CMakeCache

--- adapter2/deps/lldb/build.rs
+++ adapter2/deps/lldb/build.rs
@@ -1,4 +1,9 @@
+use regex::Regex;
+use std::collections::HashMap;
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 
 fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
@@ -6,28 +11,17 @@ fn main() {
         Ok(_) => true,
         Err(_) => false,
     };
-    let no_link_cpp_stdlib = match env::var("CARGO_FEATURE_NO_LINK_CPP_STDLIB") {
-        Ok(_) => true,
-        Err(_) => false,
-    };
 
+    // Generate C++ bindings
     let mut build_config = cpp_build::Config::new();
     build_config.include("include");
-    if no_link_cpp_stdlib {
+    if weak_linkage {
         build_config.cpp_link_stdlib(None);
     }
     build_config.build("src/lldb.rs");
 
-    println!("cargo:rerun-if-env-changed=LIBPATH");
-    if let Ok(libpath) = env::var("LIBPATH") {
-        for dir in libpath.split(';') {
-            println!("cargo:rustc-link-search=native={}", dir);
-        }
-    }
-
     if target_os == "windows" {
-        println!("cargo:rustc-link-lib=dylib=liblldb");
-        link_python(); // liblldb depends on Python too
+        strong_linkage();
     } else {
         if weak_linkage {
             if target_os == "macos" {
@@ -35,15 +29,56 @@ fn main() {
                 println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
             }
         } else {
-            println!("cargo:rustc-link-lib=dylib=lldb");
-            link_python();
+            strong_linkage();
         }
     }
 }
 
-fn link_python() {
-    println!("cargo:rerun-if-env-changed=LibPython");
-    if let Ok(libpython) = env::var("LibPython") {
-        println!("cargo:rustc-link-lib=dylib={}", libpython);
+fn strong_linkage() {
+    // Find CMakeCache
+    let mut path = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let cmakecache = loop {
+        let f = path.with_file_name("CMakeCache.txt");
+        if f.is_file() {
+            break f;
+        }
+        if !path.pop() {
+            println!("cargo:warning=Could not find CMakeCache.txt");
+            return;
+        }
+    };
+    println!("cargo:rerun-if-changed={}", cmakecache.display());
+    let config = parse_cmakecache(&cmakecache);
+
+    if let Some(value) = config.get("LLDB_LinkSearch") {
+        for path in value.split_terminator(';') {
+            println!("cargo:rustc-link-search=native={}", path);
+        }
+    } else {
+        println!("cargo:warning=LLDB_LinkSearch not set");
     }
+
+    if let Some(value) = config.get("LLDB_LinkDylib") {
+        for path in value.split_terminator(';') {
+            println!("cargo:rustc-link-lib=dylib={}", path);
+        }
+    } else {
+        println!("cargo:warning=LLDB_LinkDylib not set");
+    }
+}
+
+fn parse_cmakecache(cmakecache: &Path) -> HashMap<String, String> {
+    let mut result = HashMap::new();
+    let reader = BufReader::new(File::open(cmakecache).expect("Open file"));
+    let kvregex = Regex::new("(?mx) ^ ([A-Za-z_]+) : (STRING|FILEPATH|BOOL|STATIC|INTERNAL) = (.*)").unwrap();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if let Some(captures) = kvregex.captures(&line) {
+            let key = captures.get(1).expect("Invalid format").as_str();
+            let value = captures.get(3).expect("Invalid format").as_str();
+            result.insert(key.into(), value.into());
+        }
+    }
+    result
 }

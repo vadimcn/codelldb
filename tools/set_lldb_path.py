27caa632d4c92073ee23d56b69667a24a091b935
Vadim Chugunov <vadimcn@gmail.com>
2020-02-13 19:44:14 -0800

    Cleanup tools

--- tools/set_lldb_path.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import subprocess
-
-lldb = os.environ.get('LLDB_EXECUTABLE', 'lldb')
-
-out = subprocess.check_output([lldb, '-P'])
-sys.path.append(out.decode('utf8').strip())
-sys.path.append('.')

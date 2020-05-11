152055c703b7feddad4efa663e14046b890098a1
Vadim Chugunov <vadimcn@gmail.com>
2020-05-10 22:12:39 -0700

    Moved terminal-agent to its own module.

--- adapter/loader/main.rs
+++ adapter/loader/main.rs
@@ -3,6 +3,7 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 type Error = Box<dyn std::error::Error>;
 
 mod find_python;
+mod terminal_agent;
 
 fn main() -> Result<(), Error> {
     env_logger::Builder::from_default_env().init();
@@ -19,70 +20,12 @@ fn main() -> Result<(), Error> {
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("terminal-agent") {
-        terminal_agent(&matches)
+        terminal_agent::terminal_agent(&matches)
     } else if let Some(_) = matches.subcommand_matches("find-python") {
         find_python()
     } else {
         debug_server(&matches)
     }
-}
-
-fn terminal_agent(matches: &ArgMatches) -> Result<(), Error> {
-    use std::io::{Read, Write};
-    use std::net;
-
-    fn clear_screen() {
-        let terminal = crossterm::terminal();
-        drop(terminal.clear(crossterm::ClearType::All));
-    }
-
-    #[cfg(unix)]
-    fn purge_stdin() {
-        use std::os::unix::io::AsRawFd;
-        drop(termios::tcflush(std::io::stdin().as_raw_fd(), termios::TCIFLUSH));
-    }
-    #[cfg(windows)]
-    fn purge_stdin() {
-        use std::os::windows::io::AsRawHandle;
-        unsafe {
-            winapi::um::wincon::FlushConsoleInputBuffer(std::io::stdin().as_raw_handle());
-        }
-    }
-
-    let data;
-    #[cfg(unix)]
-    {
-        unsafe {
-            let ptr = libc::ttyname(1);
-            assert!(!ptr.is_null());
-            data = std::ffi::CStr::from_ptr(ptr).to_str()?;
-        }
-    }
-    #[cfg(windows)]
-    {
-        data = std::process::id();
-    }
-
-    let port: u16 = matches.value_of("port").unwrap().parse().unwrap();
-    let addr = net::SocketAddr::new(net::Ipv4Addr::new(127, 0, 0, 1).into(), port);
-    let mut stream = net::TcpStream::connect(addr)?;
-    write!(stream, "{}", data)?;
-
-    clear_screen();
-
-    stream.shutdown(net::Shutdown::Write)?;
-    // Wait for the other end to close connection (which will be maintained till the end of
-    // the debug session; this prevents terminal shell from stealing debuggee's input form stdin).
-    for b in stream.bytes() {
-        if let Err(_) = b {
-            break;
-        }
-    }
-
-    // Clear out any unread input buffered in stdin, so it doesn't get read by the shell.
-    purge_stdin();
-
-    Ok(())
 }
 
 fn find_python() -> Result<(), Error> {

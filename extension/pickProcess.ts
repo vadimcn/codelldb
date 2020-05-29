c30c99dca440d2a55fcd932c86dadea056f4d2c1
Vadim Chugunov <vadimcn@gmail.com>
2020-05-28 21:44:15 -0700

    PickProcess reorg.

--- extension/pickProcess.ts
+++ extension/pickProcess.ts
@@ -6,12 +6,12 @@ type ProcessItem = QuickPickItem & { pid: number };
 export async function pickProcess(context: ExtensionContext, allUsers: boolean): Promise<string> {
     return new Promise<string>(async (resolve) => {
         let showingAll = {
-            iconPath: Uri.file(context.extensionPath + '/images/checked.svg'),
-            tooltip: 'Show processes from all users.'
+            iconPath: Uri.file(context.extensionPath + '/images/users.svg'),
+            tooltip: 'Showing all processes'
         };
         let showingMy = {
-            iconPath: Uri.file(context.extensionPath + '/images/unchecked.svg'),
-            tooltip: 'Show processes from all users.'
+            iconPath: Uri.file(context.extensionPath + '/images/user.svg'),
+            tooltip: 'Showing own processes'
         };
         let qpick = window.createQuickPick<ProcessItem>();
         qpick.title = 'Select a process:';
@@ -38,18 +38,45 @@ export async function pickProcess(context: ExtensionContext, allUsers: boolean):
 }
 
 async function getProcessList(allUsers: boolean): Promise<ProcessItem[]> {
-    let is_windows = process.platform == 'win32';
     let command: string;
-    if (!is_windows) {
+    let matchLine;
+
+    if (process.platform != 'win32') {
         if (allUsers)
             command = 'ps ax';
         else
             command = 'ps x';
+
+        let regex = /^\s*(\d+)\s+.*?\s+.*?\s+.*?\s+(.*)()$/;
+        matchLine = (line: string) => {
+            let groups = regex.exec(line);
+            if (!groups)
+                return null;
+            let item = {
+                pid: parseInt(groups[1]),
+                name: groups[2],
+                description: groups[3]
+            };
+            // Filter out kernel threads
+            if (item.name.startsWith('[') && item.name.endsWith(']'))
+                return null;
+            return item;
+        };
     } else {
         if (allUsers)
             command = 'tasklist /V /FO CSV';
         else
             command = 'tasklist /V /FO CSV /FI "USERNAME eq ' + process.env['USERNAME'] + '"';
+        // name, pid, ..., window title
+        let regex = /^"([^"]*)","([^"]*)",(?:"[^"]*",){6}"([^"]*)"/;
+        matchLine = (line: string) => {
+            let groups = regex.exec(line);
+            return groups == null ? null : {
+                pid: parseInt(groups[2]),
+                name: groups[1],
+                description: groups[3]
+            };
+        };
     }
     let stdout = await new Promise<string>((resolve, reject) => {
         cp.exec(command, (error, stdout) => {
@@ -59,24 +86,10 @@ async function getProcessList(allUsers: boolean): Promise<ProcessItem[]> {
     });
     let lines = stdout.split('\n');
     let items = [];
-
-    let re: RegExp, idx: number[];
-    if (!is_windows) {
-        re = /^\s*(\d+)\s+.*?\s+.*?\s+.*?\s+(.*)()$/;
-        idx = [1, 2, 3];
-    } else {
-        // name, pid, ..., window title
-        re = /^"([^"]*)","([^"]*)",(?:"[^"]*",){6}"([^"]*)"/;
-        idx = [2, 1, 3];
-    }
     for (let i = 1; i < lines.length; ++i) {
-        let groups = re.exec(lines[i]);
-        if (groups) {
-            let pid = parseInt(groups[idx[0]]);
-            let name = groups[idx[1]];
-            let descr = groups[idx[2]];
-            let item = { label: `${pid}: ${name}`, description: descr, pid: pid };
-            items.push(item);
+        let item = matchLine(lines[i]);
+        if (item) {
+            items.push({ label: `${item.pid}: ${item.name}`, description: item.description, pid: item.pid });
         }
     }
     return items;

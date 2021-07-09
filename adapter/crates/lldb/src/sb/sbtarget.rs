345b7668bd624e4496cf78bbd012626c36934385
Vadim Chugunov <vadimcn@gmail.com>
2021-07-08 21:33:50 -0700

    DisassembleRequest

--- adapter/crates/lldb/src/sb/sbtarget.rs
+++ adapter/crates/lldb/src/sb/sbtarget.rs
@@ -221,6 +221,14 @@ impl SBTarget {
             Err(error)
         }
     }
+    pub fn get_instructions(&self, base_addr: &SBAddress, buffer: &[u8]) -> SBInstructionList {
+        let ptr = buffer.as_ptr();
+        let count = buffer.len();
+        cpp!(unsafe [self as "SBTarget*", base_addr as "SBAddress*",
+                     ptr as "void*", count as "size_t"] -> SBInstructionList as "SBInstructionList" {
+            return self->GetInstructions(*base_addr, ptr, count);
+        })
+    }
     pub fn evaluate_expression(&self, expr: &str) -> SBValue {
         with_cstr(expr, |expr| {
             cpp!(unsafe [self as "SBTarget*", expr as "const char*"] -> SBValue as "SBValue" {

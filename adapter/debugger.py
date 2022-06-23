256810dd3c7d32d23e0357ae121e0e8310ba319b
Vadim Chugunov <vadimcn@gmail.com>
2022-06-22 22:05:04 -0700

    Remove obsolete APIs

--- adapter/debugger.py
+++ adapter/debugger.py
@@ -17,20 +17,4 @@ def unwrap(obj):
 def display_html(html, title=None, position=None, reveal=False):
     codelldb.display_html(html, title, position, reveal)
 
-def register_type_callback(callback, language=None, type_class_mask=lldb.eTypeClassAny):
-    raise NotImplementedError('This API has been removed')
-
-def register_content_provider(provider):
-    raise NotImplementedError('This API has been removed')
-
-def stop_if(cond, handler):
-    import warnings
-    warnings.warn('deprecated', DeprecationWarning)
-
-    if cond:
-        handler()
-        return True
-    else:
-        return False
-
-__all__ = ['evaluate', 'wrap', 'unwrap', 'display_html', 'register_type_callback', 'register_content_provider', 'stop_if']
+__all__ = ['evaluate', 'wrap', 'unwrap', 'display_html']

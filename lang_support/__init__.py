a4f164a53a7c262a7ddc5cc4343bf988b685b275
Vadim Chugunov <vadimcn@gmail.com>
2024-10-30 22:32:07 -0700

    Fix lang_support warnings

--- lang_support/__init__.py
+++ lang_support/__init__.py
@@ -7,13 +7,16 @@ log = logging.getLogger(__name__)
 def __lldb_init_module(debugger, internal_dict):  # pyright: ignore
     adapter_settings = internal_dict['adapter_settings']
     langs = adapter_settings.get('sourceLanguages', [])
+    log.info('languages: {}'.format(langs))
     for lang in langs:
-        try:
-            ns = __import__('lang_support', fromlist=[lang])
-            getattr(ns, lang).__lldb_init_module(debugger, internal_dict)
-        except ImportError:
-            pass
-        except Exception as e:
-            message = 'Failed to initialize language support for {}:'.format(lang)
-            log.exception(message)
-            print(message, str(e))
+        ns = __import__('lang_support', fromlist=[lang])
+        mod = getattr(ns, lang, None)
+        if mod is None:
+            log.debug('No lang support found for {}'.format(lang))
+        else:
+            try:
+                mod.__lldb_init_module(debugger, internal_dict)
+            except Exception as e:
+                message = 'Failed to initialize language support for {}'.format(lang)
+                log.exception(message)
+                print(message, str(e))

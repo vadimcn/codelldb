f52aef20a7a8a7d427b577ff6b3f1d121f05c004
Vadim Chugunov <vadimcn@gmail.com>
2024-11-10 21:02:08 -0800

    Deduplicate sourceLanguages

--- lang_support/__init__.py
+++ lang_support/__init__.py
@@ -6,7 +6,7 @@ log = logging.getLogger(__name__)
 
 def __lldb_init_module(debugger, internal_dict):  # pyright: ignore
     adapter_settings = internal_dict['adapter_settings']
-    langs = adapter_settings.get('sourceLanguages', [])
+    langs = set(adapter_settings.get('sourceLanguages', []))
     log.info('languages: {}'.format(langs))
     for lang in langs:
         ns = __import__('lang_support', fromlist=[lang])

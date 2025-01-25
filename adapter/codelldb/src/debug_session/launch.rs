cf369a8ab8489db7fbbbfc1d24970a3ecdcd19b3
Vadim Chugunov <vadimcn@gmail.com>
2025-01-25 00:20:06 -0800

    WIP

--- adapter/codelldb/src/debug_session/launch.rs
+++ adapter/codelldb/src/debug_session/launch.rs
@@ -151,14 +151,9 @@ impl super::DebugSession {
 
         let launch_result: Result<SBProcess, Error> = (|| {
             if let Some(commands) = &args.process_create_commands {
-                // let target = self.with_sync_mode(|| -> Result<SBTarget, Error> {
-                //     self.exec_commands("processCreateCommands", commands)?;
-                //     Ok(self.debugger.selected_target())
-                // })?;
                 self.exec_commands("processCreateCommands", commands)?;
-                let target = self.debugger.selected_target();
                 if self.target_is_dummy {
-                    self.set_target(target);
+                    self.set_target(self.debugger.selected_target());
                 }
                 Ok(self.target.process())
             } else {
@@ -195,25 +190,7 @@ impl super::DebugSession {
 
         self.terminate_on_disconnect = true;
 
-        let process_state = self.target.process().state();
-        debug!("{:?}", process_state);
-
-        // if !process_state.is_alive() {
-        //     self.notify_process_terminated();
-        // }
-
-        // else if args.process_create_commands.is_none() {
-        //     // API launch
-        //     if launch_info.launch_flags().intersects(LaunchFlag::StopAtEntry) {
-        //         // LLDB sometimes loses the initial stop event.
-        //         self.notify_process_stopped();
-        //     }
-        // } else {
-        //     // process_create_commands launch
-        //     // if !process_state.is_running() {
-        //     //     self.notify_process_stopped();
-        //     // }
-        // }
+        debug!("Process state: {:?}", self.target.process().state());
 
         self.common_post_run(args.common)?;
 
@@ -269,14 +246,9 @@ impl super::DebugSession {
         }
 
         let process = if let Some(commands) = &args.process_create_commands {
-            // let target = self.with_sync_mode(|| -> Result<SBTarget, Error> {
-            //     self.exec_commands("processCreateCommands", commands)?;
-            //     Ok(self.debugger.selected_target())
-            // })?;
             self.exec_commands("processCreateCommands", commands)?;
-            let target = self.debugger.selected_target();
             if self.target_is_dummy {
-                self.set_target(target);
+                self.set_target(self.debugger.selected_target());
             }
             self.target.process()
         } else {

58cd1050936f7f49a47900912f38454ffd1e075c
Vadim Chugunov <vadimcn@gmail.com>
2025-11-26 20:27:45 -0800

    Cleanup

--- tools/linux-builder.sh
+++
@@ -1,9 +0,0 @@
-LLDB_ROOT=$1
-docker run -it --rm --name=linux-builder \
-    -v  $(realpath $(dirname $BASH_SOURCE)/..):/codelldb:ro \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    -v /etc/localtime:/etc/localtime:ro \
-    -v /etc/passwd:/etc/passwd:ro \
-    -v /etc/group:/etc/group:ro \
-    -v $LLDB_ROOT:/lldb:ro \
-    vadimcn/linux-builder
